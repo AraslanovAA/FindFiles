c5916bd8e631a4214af421a9887f030d54d26482
AraslanovAA <araslanov.anton.p@gmail.com>
2019-02-19 22:56:16 +0300

    Реализованы функции копирования/перемещения с возможностями заглядывать/не заглядывать в подкаталоги. Файлы выибраются по указанным расширеням

--- FindByExtension/FindFiles.py
+++ FindByExtension/FindFiles.py
@@ -1,26 +1,33 @@
 import os
 import shutil
-#для GUI надо чекбоксы: залезать в подкаталоги
-#радиобаттоны: копировать/перемещать файлы
-# opendirectory enddirectory
-# эдит для ввода перебираемых расширений и кнопка чтоб все запустить как минимум
-StartDirectory = "C:\с телефона"
-DestinationDirectory = "C:\\Users\\User\PycharmProjects\Destination"
+#todo: случай когда файл с атким именем уже и меется в папке назначения, хотя очевидно задача не для этого предназнаена
+def ReplaceByExtencionWhichCat(startDirectory,endDirectory,Extencions, do):
+    for f in os.listdir(startDirectory):
+        for i in Extencions:
+            if f.endswith(i):
+                dest = os.path.join(endDirectory, f)
+                path = os.path.join(startDirectory, f)
+                if do == 1:
+                    shutil.copy(path, dest)
+                else:
+                    shutil.move(path,dest)
 
-#files = [f for f in os.listdir(StartDirectory) код на случай если не требуется заглядывать в подкаталоги
-#         if ((f.endswith('.txt')) or(f.endswith('.png')))]  прикручу этот код, когда добавлю GUI
 
-
-
-#получаем полный путь всех элементов заданного типа в указанном каталоге и подкаталогах
-AllUsedNames = []
-for d, dirs, files in os.walk(StartDirectory):
-    for f in files:
-        if f.endswith('.jpg'):
-            path = os.path.join(d,f) # формирование начального адреса
-            for Elem in AllUsedNames:
-                 if (Elem == f):#проверяем, не использовалось ли это имя ранее, чтобы не потерять файл
-                    f = f.replace('.jpg','(1).jpg')
-            dest = os.path.join(DestinationDirectory,f)
-            shutil.move(path,dest)
-            AllUsedNames.append(f) # запомниаем сохранённые имена
+def ReplaceByExtencion(startDirectory,endDirectory, Extencions,do):
+    '''получаем полный путь всех элементов заданного типа в указанном каталоге и подкаталогах
+     перемещаем найденные элементы в конечную директорию'''
+    AllUsedNames = []
+    for d, dirs, files in os.walk(startDirectory):
+        for i in Extencions:
+            for f in files:
+                if f.endswith(i):
+                    path = os.path.join(d,f) # формирование начального адреса
+                    for Elem in AllUsedNames:
+                         if (Elem == f):#проверяем, не использовалось ли это имя ранее, чтобы не потерять файл
+                            f = f.replace(i,'(1)'+i)
+                    dest = os.path.join(endDirectory,f)
+                    if do == 1:
+                        shutil.copy(path,dest)
+                    else:
+                        shutil.move(path,dest)
+                    AllUsedNames.append(f) # запомниаем сохранённые имена
